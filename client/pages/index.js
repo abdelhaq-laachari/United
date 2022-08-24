b119f8bfdd651c563756a44a9e6612f3bb346638
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-08-24 16:19:34 +0100

    footer done

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 
 export default function Home() {
   return (
@@ -326,6 +327,7 @@ export default function Home() {
         </div>
       </div>
       {/* our partners section end */}
+      <Footer/>
     </div>
   );
 }
