8dcf55b6d705f93a1f64ffc37a1b0c47f4cee685
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-08-24 15:18:02 +0100

    section six done

--- client/pages/index.js
+++ client/pages/index.js
@@ -278,21 +278,52 @@ export default function Home() {
       {/* who we are section end */}
       {/* our partners section start */}
       <div className={styles.section_six}>
-        {/* top shape start */}
-        <div className={styles.custom_shape_divider_top_1661344935}>
-          <svg
-            data-name="Layer 1"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1200 120"
-            preserveAspectRatio="none"
-          >
-            <path
-              d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-              className={styles.shape_fill}
-            ></path>
-          </svg>
+        <div className={styles.section_six_title}>
+          <img src="/images/w.svg" alt="" />
+          <h1 className="">Our partners</h1>
         </div>
-        {/* top shape end */}
+        <div className={styles.section_six_shape}>
+          {/* top shape start */}
+          <div className={styles.custom_shape_divider_top_1661344935}>
+            <svg
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1200 120"
+              preserveAspectRatio="none"
+            >
+              <path
+                d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+                className={styles.shape_fill}
+              ></path>
+            </svg>
+          </div>
+          {/* top shape end */}
+          <div className={styles.section_six_images}>
+            <img className={styles.partners_lg} src="/images/ocp.png" alt="" />
+            <img className={styles.partners_logo} src="/images/cr.png" alt="" />
+            <img className={styles.partners_lg} src="/images/ms.png" alt="" />
+            <img
+              className={styles.partners_logo}
+              src="/images/um6p.png"
+              alt=""
+            />
+          </div>
+          {/* bottom shape start */}
+          <div className={styles.custom_shape_divider_bottom_1661350544}>
+            <svg
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1200 120"
+              preserveAspectRatio="none"
+            >
+              <path
+                d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+                className={styles.shape_fill}
+              ></path>
+            </svg>
+          </div>
+          {/* bottom shape end */}
+        </div>
       </div>
       {/* our partners section end */}
     </div>
